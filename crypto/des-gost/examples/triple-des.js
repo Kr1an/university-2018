18154727db529301533f5336abb705290480a252
Anton Krinitsyn <kr1an@hotmail.com>
2018-09-25 08:21:09 +0300

    add crypto double, triple des

---
+++ crypto/des-gost/examples/triple-des.js
@@ -0,0 +1,23 @@
+#!/bin/node
+const crypto = require('crypto');
+const fs = require('fs');
+const nthDes = require('../lib/nth-des');
+
+
+
+(function main() {
+	const keys = new Array(3).fill(null).map(x => crypto.randomBytes(32).toString('hex'))
+	console.log('### KEYS generated:');
+	console.log(...keys);
+	const fileContent = fs.readFileSync('input.txt').toString();
+	console.log('### FILE content(input.txt):');
+	console.log(fileContent);
+	const encrypted = nthDes.encrypt(fileContent, keys);
+	fs.writeFileSync('encrypted.txt', encrypted);
+	console.log('### ENCRYPTED content. Written to file(encrypted.txt)');
+	console.log(encrypted);
+	const decrypted = nthDes.decrypt(encrypted, keys);
+	fs.writeFileSync('decrypted.txt', decrypted);
+	console.log('### DECRYPTED content. Written to file(decrypted.txt)');
+	console.log(decrypted);
+})()
