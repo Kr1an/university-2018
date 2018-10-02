657031f9c9c9e6b8abaab61a9e95bc7947393b4b
Ubuntu <ubuntu@ip-10-0-0-233.eu-west-1.compute.internal>
2018-10-02 20:45:43 +0000

    fix des lab

--- crypto/des/double-des.js
+++ crypto/des/double-des.js
@@ -1,23 +1,57 @@
 #!/bin/node
-const crypto = require('crypto');
+const { DES, enc } = require('crypto-js');
 const fs = require('fs');
-const nthDes = require('./lib/nth-des');
 
+const encrypt = (data, key1, key2) => {
+	const encryptedOnce = DES.encrypt(data, key1).toString();
+	const encryptedTwice = DES.encrypt(encryptedOnce, key2).toString();
+	
+	return encryptedTwice
+}
 
+const decrypt = (data, key1, key2) => {
+	const decryptedOnce = DES.decrypt(data, key2).toString(enc.Utf8);
+	const decryptedTwice = DES.decrypt(decryptedOnce, key1).toString(enc.Utf8);
 
-(function main() {
-	const keys = new Array(2).fill(null).map(x => crypto.randomBytes(32).toString('hex'))
-	console.log('### KEYS generated:');
-	console.log(...keys);
-	const fileContent = fs.readFileSync('input.txt').toString();
-	console.log('### FILE content(input.txt):');
-	console.log(fileContent);
-	const encrypted = nthDes.encrypt(fileContent, keys);
-	fs.writeFileSync('encrypted.txt', encrypted);
-	console.log('### ENCRYPTED content. Written to file(encrypted.txt)');
-	console.log(encrypted);
-	const decrypted = nthDes.decrypt(encrypted, keys);
-	fs.writeFileSync('decrypted.txt', decrypted);
-	console.log('### DECRYPTED content. Written to file(decrypted.txt)');
-	console.log(decrypted);
-})()
+	return decryptedTwice
+}
+
+const createDDos = (key1, key2) => {
+	return {
+		encrypt: data => encrypt(data, key1, key2),
+		decrypt: data => decrypt(data, key1, key2)
+	}
+
+}
+
+const tryReadFromFile = (filePath, otherwhiseValue) => {
+	if (!fs.existsSync(filePath)) {
+		return otherwhiseValue
+	}
+	return fs.readFileSync(filePath).toString()
+}
+
+function main() {
+	const key1 = tryReadFromFile('key1.txt') || 'Secret Key 1'
+	const key2 = tryReadFromFile('key2.txt') || 'Secret Key 2'
+	const data = tryReadFromFile('input.txt') || 'Not Encrypted Message'
+
+	console.log('double des')
+
+	console.log('keys:', [key1, key2])
+	console.log('data:', data)
+
+	const ddos = createDDos(key1, key2)
+
+	const encrypted = ddos.encrypt(data)
+	const decrypted = ddos.decrypt(encrypted)
+
+	console.log('encrypted:', encrypted)
+	console.log('decrypted:', decrypted)
+
+	fs.writeFileSync('encrypted.txt', encrypted)
+	fs.writeFileSync('decrypted.txt', decrypted)
+}
+main()
+
+module.exports = createDDos
--- crypto/des/triple-des.js
+++ crypto/des/triple-des.js
@@ -1,23 +1,58 @@
-#!/bin/node
-const crypto = require('crypto');
+const { DES, enc } = require('crypto-js');
 const fs = require('fs');
-const nthDes = require('./lib/nth-des');
 
+const encrypt = (data, key1, key2) => {
+	const encryptedOnce = DES.encrypt(data, key1).toString();
+	const encryptedTwice = DES.encrypt(encryptedOnce, key2).toString();
+	const encryptedThird = DES.encrypt(encryptedTwice, key1).toString();
 
+	return encryptedThird
+}
 
-(function main() {
-	const keys = new Array(3).fill(null).map(x => crypto.randomBytes(32).toString('hex'))
-	console.log('### KEYS generated:');
-	console.log(...keys);
-	const fileContent = fs.readFileSync('input.txt').toString();
-	console.log('### FILE content(input.txt):');
-	console.log(fileContent);
-	const encrypted = nthDes.encrypt(fileContent, keys);
-	fs.writeFileSync('encrypted.txt', encrypted);
-	console.log('### ENCRYPTED content. Written to file(encrypted.txt)');
-	console.log(encrypted);
-	const decrypted = nthDes.decrypt(encrypted, keys);
-	fs.writeFileSync('decrypted.txt', decrypted);
-	console.log('### DECRYPTED content. Written to file(decrypted.txt)');
-	console.log(decrypted);
-})()
+const decrypt = (data, key1, key2) => {
+	const decryptedOnce = DES.decrypt(data, key1).toString(enc.Utf8);
+	const decryptedTwice = DES.decrypt(decryptedOnce, key2).toString(enc.Utf8);
+	const decryptedThird = DES.decrypt(decryptedTwice, key1).toString(enc.Utf8);
+
+	return decryptedThird
+}
+
+const createTDes = (key1, key2) => {
+	return {
+		encrypt: data => encrypt(data, key1, key2),
+		decrypt: data => decrypt(data, key1, key2)
+	}
+
+}
+
+const tryReadFromFile = (filePath, otherwhiseValue) => {
+	if (!fs.existsSync(filePath)) {
+		return otherwhiseValue
+	}
+	return fs.readFileSync(filePath).toString()
+}
+
+function main() {
+	const key1 = tryReadFromFile('key1.txt') || 'Secret Key 1'
+	const key2 = tryReadFromFile('key2.txt') || 'Secret Key 2'
+	const data = tryReadFromFile('input.txt') || 'Not Encrypted Message'
+
+	console.log('triple des')
+
+	console.log('keys:', [key1, key2])
+	console.log('data:', data)
+
+	const ddes = createTDes(key1, key2)
+
+	const encrypted = ddes.encrypt(data)
+	const decrypted = ddes.decrypt(encrypted)
+
+	console.log('encrypted:', encrypted)
+	console.log('decrypted:', decrypted)
+
+	fs.writeFileSync('encrypted.txt', encrypted)
+	fs.writeFileSync('decrypted.txt', decrypted)
+}
+main()
+
+module.exports = createTDes
